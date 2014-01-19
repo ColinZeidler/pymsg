f14490cfd3ffacd749886f18461bbd7c087eaef3
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-01-19 15:55:03 -0500

    client map reversed storing addr as keys. added msg_control function

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ def main():
 	print conns.PORT
 
 	Thread(target=accept_cons).start()
+	Thread(target=msg_control).start()
 	
 	while True:
 		sleep(2)
@@ -29,17 +30,28 @@ def accept_cons():
 		conns.sock.listen(1)
 		conn, addr = conns.sock.accept()
 		print "Connected to ", addr
-		send(conns.sock, conn, "Welcome")
+		send(conns.sock.getsockname()[0], conn, "Welcome")
 
 		conn.setblocking(0) #set the socket to non blocking
 		#should allow checking for data, and continuing if none
 		#preventing concurrent access
 		lock.acquire()
-		clients[conn] = addr
+		clients[addr] = conn
 		lock.release()
 
 def send(sender, reciever, msg):
-	reciever.send(sender.getsockname()[0] + ": " + msg)
+	reciever.send(sender + ": " + msg)
+
+def msg_control():
+	msg = conns.sock.recv(128)
+	if msg[:3] == "/dc":
+		#remove the client from the dict
+		pass
+	else:
+		#loop through all clients and send the message out
+		#for person in client.keys():
+			
+		pass
 
 if __name__ == '__main__':
 	#do the normal stuff
