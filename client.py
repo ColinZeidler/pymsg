be24fb58a85488fcd15b3897ab1e9975574a4aa4
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-01-19 15:29:15 -0500

    connect to local and print the welcome message

--- client.py
+++ client.py
@@ -1,7 +1,24 @@
 #author Colin Zeidler
+from time import sleep
+from threading import Thread, Lock
+import socket
+import conns
+
+HOST = 'localhost'
 
 def main():
 	print "Hello world"
+	connect(HOST)
+
+def connect(server):
+	conns.sock.connect((server, conns.PORT))
+
+	msg = conns.sock.recv(128)
+	print msg
+	pass
+
+def listen():
+	pass
 
 if __name__ == "__main__":
 	#do the normal stuff
