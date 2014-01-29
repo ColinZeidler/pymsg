697b1d87ab4c11c59391d4bdedb503db006ecea9
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-01-29 16:36:22 -0500

    moved msg parsing out of server.py

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 #author Colin Zeidler
 from time import sleep
 from threading import Thread, Lock
-import socket
-import conns
+import socket, sys
+import conns, messages
 
 def main():
 	print "Starting server"
-	print clients
+	print conns.clients
 	print conns.PORT
 
 	Thread(target=accept_cons).start()
@@ -25,7 +25,11 @@ def main():
 #accepts new connections and adds them to the server
 def accept_cons():
 	print "Started accepting"
-	conns.sock.bind((conns.HOST, conns.PORT))
+	try:
+		conns.sock.bind((conns.HOST, conns.PORT))
+	except:
+		print "Unable to bind socket"
+		sys.exit(0)
 	while True: #change this so that the program can exit cleanly
 		conns.sock.listen(1)
 		conn, addr = conns.sock.accept()
@@ -48,22 +52,7 @@ def msg_control():
 				msg = con.recv(128)
 			except:
 				continue
-			if msg[:3] == "/dc":
-				#remove the client from the dict
-				print "disconnecting client"
-				for person in conns.clients.keys():
-					conns.send(conns.SERVER_STR, person, conns.clients.get(con)[0] + " Disconnected")
-				con.close()
-				del conns.clients[con]
-				#do not want to broadcast the /dc command so continue
-				continue
-			#loop through all clients and send the message out
-			for person in conns.clients.keys():
-				if person == con:
-					#do not send the message back to the sender
-					continue
-				conns.send(conns.clients.get(con)[0], person, msg)
-			
+			messages.parse_msg(msg, con)
 		lock.release()
 
 if __name__ == '__main__':
