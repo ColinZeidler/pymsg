1c75cda04ec6271488c90da511f1b5ba92e4de56
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-01-29 16:20:07 -0500

    added need for the sending con to be passed in

--- messages.py
+++ messages.py
@@ -7,12 +7,12 @@ commands = 	{'/dc':2,   #disconnect client from server,	/dc {reason/msg}
 			'/to':3}    #send to given channel, 			/to {channel} {msg}
 hasMsg = ['/dc', '/to']
 
-def parse_msg(msg):
+def parse_msg(msg, con):
 	strings = msg.split(" ")
 	cmd = strings[0]
 	if cmd in commands:
 		if cmd == "/dc":
-			server_dc(" ".join(strings[commands.get(cmd):]))
+			server_dc(" ".join(strings[commands.get(cmd):]), con)
 		elif cmd == "/stop":
 			pass
 		elif cmd == "/join":
@@ -22,9 +22,12 @@ def parse_msg(msg):
 		elif cmd == "/to":
 			pass
 	else: #msg does not contain a command
-		conns.send(msg)
+		for person in conns.clients.keys():
+			if person == con:
+				continue
+			conns.send(conns.clients.get(con)[0], person, msg)
 
-def server_dc(msg):
+def server_dc(msg, con):
 	print "disconnecting client"
 	for person in conns.clients.keys():
 		conns.send(conns.SERVER_STR, person,
