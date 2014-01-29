5f8784651b59110bb5d0f2b0a3d2101288fbd83d
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-01-29 17:40:43 -0500

    added functionality to message parser

--- messages.py
+++ messages.py
@@ -1,11 +1,11 @@
 import conns
-import socket
+import socket, sys
 
-commands = 	{'/dc':2,   #disconnect client from server,	/dc {reason/msg}
-			'/stop':2,  #tell server to stop running,		/stop {passwd}
-			'/join':2,  #join given channel,				/join {channel}
-			'/leave':2, #leave given channel,				/leave {channel}
-			'/to':3}    #send to given channel, 			/to {channel} {msg}
+commands = 	{'/dc':1,   #disconnect client from server,	/dc {reason/msg}
+			'/stop':1,  #tell server to stop running,		/stop {passwd}
+			'/join':1,  #join given channel,				/join {channel}
+			'/leave':1, #leave given channel,				/leave {channel}
+			'/to':2}    #send to given channel, 			/to {channel} {msg}
 hasMsg = ['/dc', '/to']
 
 def parse_msg(msg, con):
@@ -15,11 +15,11 @@ def parse_msg(msg, con):
 		if cmd == "/dc":
 			server_dc(" ".join(strings[commands.get(cmd):]), con)
 		elif cmd == "/stop":
-			pass
+			stop(strings[1])
 		elif cmd == "/join":
-			pass
+			join(strings[1])
 		elif cmd == "/leave":
-			pass
+			leave(strings[1])
 		elif cmd == "/to":
 			pass
 	else: #msg does not contain a command
@@ -30,13 +30,15 @@ def parse_msg(msg, con):
 
 def server_dc(msg, con):
 	print "disconnecting client"
-	for person in conns.clients.keys():
-		conns.send(conns.SERVER_STR, person, conns.clients.get(con)[0] + " Disconnected")
+	broadcast_all(conns.SERVER_STR, conns.clients.get(con)[0] + " Disconnected")
 	con.close()
 	del conns.clients[con]
 
 def stop(passwd):
-	pass
+	if passwd == conns.PASS:
+		broadcast_all("/dc ", "")
+		conns.sock.close()
+		sys.exit(0)
 
 def join(room):
 	pass
@@ -46,3 +48,8 @@ def leave(room):
 
 def to(room, msg):
 	pass
+
+
+def broadcast_all(sender, msg):
+	for person in conns.clients.keys():
+		conns.send(sender, person, msg)
